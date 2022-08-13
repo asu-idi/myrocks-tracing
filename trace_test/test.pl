d25b92efef3987ee501e1ac36e945b4977ba4441
bilyz <zhuangzhutao@gmail.com>
2022-08-13 17:57:09 +0800

    update tpch learning notes and scripts

--- trace_test/test.pl
+++ trace_test/test.pl
@@ -3,7 +3,7 @@
 
 
 
-$load_output = `mysql -u root -e "use test; LOAD DATA INFILE '~/tpc-h/TPC-H_Tools_v3.0.0/dbgen/lineitem.tbl.u1' INTO TABLE test.lineitem FIELDS TERMINATED BY '|';"`;
+$load_output = `mysql -u root -e "use test; LOAD DATA LOCAL INFILE '~/tpc-h/TPC-H_Tools_v3.0.0/dbgen/lineitem.tbl.u1' INTO TABLE LINEITEM FIELDS TERMINATED BY '|';"`;
 
 
 print $load_output;
